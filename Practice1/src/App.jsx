a6868ac4f04936fe844728f80da7102f59aacd29
karthik-769 <karthikreddy14453@gmail.com>
2026-02-17 19:13:18 +0530

    intial commit

---
+++ Practice1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id='parent'>
+      <div id='child'>
+        <h1>I'm the child</h1>
+      </div>
+      <div id="child1">
+        <h1>I'm the child1</h1>
+      </div>      
+    </div>
+  )
+}
+
+export default App
